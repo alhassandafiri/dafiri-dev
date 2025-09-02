6c549bc924295ce0621a8098a63effd60c3bb11a
Harson <hassandaf07@gmail.com>
2025-09-02 23:34:51 +0100

    fix case sensitive import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/styles/globals.css'
+import './assets/styles/Globals.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
