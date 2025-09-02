3b61f1793c0fcf2c24df01c8fcd4274162d1aa1e
Harson <hassandaf07@gmail.com>
2025-09-02 14:46:59 +0100

    rename technology names in data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ export const projectsData = [
   {
   title: 'Portfolio Website',
   description: 'A clean, minimalist portfolio built with React and CSS Modules to showcase my skills and projects. Designed to be fully responsive and performant.',
-  technologies: ['React (JS)', 'Vite', 'CSS Modules'],
+  technologies: ['React.js', 'Vite', 'SCSS'],
   githubLink: 'https://github.com/alhassandafiri/personal-portfolio-website',
   liveLink: 'http://localhost:5173/',
   progress: 100
@@ -11,7 +11,7 @@ export const projectsData = [
 {
   title: ' ratewise.',
   description: 'A sleek, real-time currency converter and financial data visualizer. Instantly convert currencies, browse live exchange rates, and analyze historical data with interactive charts.',
-  technologies: ['React (JS)', 'Laravel', 'Recharts', 'CSS Modules', 'Framer Motion'],
+  technologies: ['React.js', 'Laravel', 'Recharts', 'CSS Modules', 'Framer Motion'],
   githubLink: 'https://github.com/alhassandafiri/currency-converter',
   liveLink: 'http://localhost:5175/',
   progress: 100
@@ -19,7 +19,7 @@ export const projectsData = [
 {
   title: 'JobTrackr',
   description: 'A modern job application tracking dashboard with a Kanban board interface and analytics. Track applications across different stages and view insights through charts.',
-  technologies: ['React (JS)', 'Tailwind CSS', 'Laravel', 'PostgreSQL', 'Chart.js', 'React DnD'],
+  technologies: ['React.js', 'Tailwind', 'Laravel', 'PostgreSQL', 'Chart.js', 'React DnD'],
   githubLink: 'https://github.com/alhassandafiri/jobtrackr',
   liveLink: 'http://localhost:5173/',
   progress: 25
